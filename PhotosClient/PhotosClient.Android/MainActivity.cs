8e3ddba119467c5cffab92f23538368ea8ccf862
Evgeny Muryshkin <evmuryshkin@gmail.com>
2021-07-26 21:28:03 +1000

    icons, screenshots, production release

--- PhotosClient/PhotosClient.Android/MainActivity.cs
+++ PhotosClient/PhotosClient.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using System.Net;
 
 namespace PhotosClient.Droid
 {
-    [Activity(Label = "PhotosClient", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Home Schooling", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
